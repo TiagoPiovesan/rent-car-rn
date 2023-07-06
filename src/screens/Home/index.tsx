ece0ae8bf3ab27183e06f49be18583c5d2aedaca
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2023-07-05 23:19:35 -0300

    feat: create header and add assets

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,30 @@
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars
+} from "./styles";
+import { StatusBar } from 'react-native'
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from '../../assets/logo.svg';
 
 export function Home() {
   return (
     <Container>
-      <Title>
-daasdasdaioaJSDOISJDOAIDSJOAIDSJO
-      </Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>
+            Total de 12 Carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   )
 }
