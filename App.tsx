573a85645cd72f8c758024fdd71e9f2f3982d470
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2023-07-05 22:26:05 -0300

    feat: first commit

---
+++ App.tsx
@@ -0,0 +1,51 @@
+import React, { useCallback, useEffect } from 'react';
+import { Home } from './src/screens/Home';
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo'
+import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from '@expo-google-fonts/inter'
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+      Archivo_400Regular,
+      Archivo_500Medium,
+      Archivo_600SemiBold,
+      Inter_400Regular,
+      Inter_500Medium,
+      Inter_600SemiBold
+    })
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <View
+        style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <Home />
+      </View>
+    </ThemeProvider>
+  );
+}
