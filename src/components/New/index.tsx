14ceb7ac677f7de3bf42fba32958c9eb2f7e0d07
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2023-07-16 18:20:14 -0300

    feat: create component car item

---
+++ src/components/New/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import { Container } from './styles'
+
+export default function Car() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,8 +8,30 @@ import { StatusBar } from 'react-native'
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from '../../assets/logo.svg';
+import Car from "../../components/Car";
 
 export function Home() {
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Caupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120
+    },
+    thumbnail: "https://img.sm360.ca/ir/w640h390c/images/newcar/ca/2022/audi/rs-7-sportback/base-rs-7-sportback/sedan/exteriorColors/2022_audi_rs-7-sportback_base_001_t3t3.png"
+  }
+
+  const carDataTwo = {
+    brand: 'Porche',
+    name: 'PANAMERA',
+    rent: {
+      period: 'AO DIA',
+      price: 300
+    },
+    thumbnail: "https://www.pngplay.com/wp-content/uploads/13/Porsche-Panamera-No-Background.png"
+  }
+
+
   return (
     <Container>
       <StatusBar
@@ -25,6 +47,8 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={ carDataOne } />
+      <Car data={ carDataTwo } />
     </Container>
   )
 }
